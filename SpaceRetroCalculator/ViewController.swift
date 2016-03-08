f9ecf292477f6c387c381edd72af162833b78f57
Tanveer Bashir <tan.m.bashir@gmail.com>
2016-03-08 18:03:18 -0500

    initial

--- SpaceRetroCalculator/ViewController.swift
+++ SpaceRetroCalculator/ViewController.swift
@@ -7,19 +7,105 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    enum Operations: String{
+        case Devide = "/"
+        case Myltiply = "*"
+        case Subtract = "-"
+        case Addition = "+"
+        case Empty = "Empty"
+    }
+    
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    var audioPlayer:AVAudioPlayer!
+    
+    var runningNummber = ""
+    var rightValue = ""
+    var leftValue = ""
+    var result:String = ""
+    var operations:Operations = Operations.Empty
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        operations = Operations.Empty
+        let path = NSBundle.mainBundle().URLForResource("btn", withExtension: "wav")
+        do{
+            try audioPlayer = AVAudioPlayer(contentsOfURL: path!)
+        } catch let error as NSError{
+            print("\(error.localizedDescription)")
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
     }
-
-
+    
+    @IBAction func digitPressed(sender: UIButton) {
+       playSound()
+        runningNummber += "\(sender.tag)"
+        displayLabel.text = runningNummber
+    }
+    
+    @IBAction func subtractButtonPressed(sender: UIButton) {
+        playSound()
+        operations = Operations.Subtract
+        print("\(operations)")
+        processesOperation(Operations.Subtract)
+    }
+    
+    @IBAction func additionButtonPressed(sender: UIButton) {
+        playSound()
+        operations = Operations.Addition
+        print("\(operations)")
+        processesOperation(Operations.Addition)
+    }
+    
+    @IBAction func devideButtonPressed(sender: UIButton) {
+        operations = Operations.Devide
+        print("\(operations)")
+        processesOperation(Operations.Devide)
+    }
+    
+    @IBAction func multiplyButtonPressed(sender: UIButton) {
+        playSound()
+        operations = Operations.Myltiply
+        print("\(operations)")
+        processesOperation(Operations.Myltiply)
+    }
+    
+    @IBAction func equalButtonPressed(sender: UIButton) {
+        playSound()
+        processesOperation(operations)
+    }
+    
+    func playSound(){
+        if audioPlayer.playing{
+            audioPlayer.stop()
+        }
+        audioPlayer.play()
+    }
+    
+    func processesOperation(op: Operations) {
+        if operations != Operations.Empty {
+            if runningNummber != ""{
+                rightValue = runningNummber
+                runningNummber = ""
+                if operations == Operations.Addition {
+                    result = "\(Double(leftValue)! + Double(rightValue)!)"
+                }
+            }
+        } else {
+            rightValue = runningNummber
+            runningNummber = ""
+            operations = op
+        }
+        displayLabel.text = result
+        
+    }
 }
 
